c2b829742fab362b644c80006bcec65081382194
mrfirdauss-20 <16520456@std.stei.itb.ac.id>
2021-01-27 09:52:42 +0700

    times

--- app.js
+++ app.js
@@ -24,6 +24,17 @@ app.get("/", (req, res) => {
     res.send("Hello from ROOOOOT")
 })
 
+app..get('/times', (req, res) => res.send(showTimes()))
+
+showTimes = () => {
+    let result = '';
+    const times = process.env.TIMES || 5;
+    for (i = 0; i < times; i++) {
+        result += i + ' ';
+    }
+    return result;
+}
+
 app.listen(PORT, () => {
     console.log('Running on', PORT);
 });
